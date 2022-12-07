9a01aa701e6505a4d0107d50de7b3f07328f01e6
Malo Lelandais <malo.l@padoa-group.com>
2022-12-07 17:49:38 +0100

    add: connection with sqldb

--- index.py
+++ index.py
@@ -1,8 +1,16 @@
-from flask import Flask
-
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
 app = Flask(__name__)
-
-
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'mydb'
+ 
+mysql = MySQL(app)
+#commande a mettre pour envoyer en db
+#mysql.connection.commit()
 @app.route('/')
 def hello():
     return '<h1>Hello, World!</h1>'
