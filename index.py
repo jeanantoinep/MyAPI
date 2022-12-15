3a3dffbed132901a939fc4e00767cb11c8123459
MaloLelandais <leland_m@etna-alternance.net>
2022-12-15 15:59:07 +0100

    add update videos

--- index.py
+++ index.py
@@ -358,6 +358,53 @@ def get_videos(id):
         user = cursor.fetchone()
         userKeys = ('id', "username", "pseudo", "created_at", "email")
 
+
+@app.route('/video/<id>', methods=['PUT'])
+def update_video(name,user):
+    if user[0] != int(id):
+        return Response(response=json.dumps({'message': "Unauthorized"}), status=401, content_type="application/json")
+    
+    invalid = []
+    if not (isinstance(request.form['name'], str)):
+        invalid.append('name')
+    if not request.files['video'] or not request.files['video'].mimetype.startswith('video/'):
+        invalid.append('video')
+    
+    if len(invalid) > 0:
+        response = {"message" : "Bad Request", "code": 10001, "data": invalid}
+        return Response(response=json.dumps(response), status=400, content_type="application/json")
+
+    file = request.files['video']
+
+    print(file)
+    filename = request.form['name']
+    print(filename)
+    file_extension = file.filename.split('.')[-1]
+    if not os.path.isdir(f"./videos/{id}"):
+        os.makedirs(f"./videos/{id}")
+    
+    if os.path.isfile(f"./videos/{id}/{filename}.{file_extension}"):
+        i=1
+        while os.path.isfile(f"./videos/{id}/{filename}({i}).{file_extension}"):
+            i+=1
+        filename += f"({i})"
+    
+    save_path = f"./videos/{id}" + f"/{filename}.{file_extension}"
+    file.save(os.path.join(save_path))
+
+    cursor = mysql.connection.cursor()
+    insert_video = f"INSERT INTO video (name, user_id, source, created_at, view, enabled) VALUES ('{filename}', '{id}', '{save_path}', '{date.today()}', '0', '1')"
+    cursor.execute(insert_video)
+    mysql.connection.commit()
+
+    get_video = f"SELECT id, source, created_at, view, enabled FROM video WHERE source='{save_path}'"
+    cursor.execute(get_video)
+    video = cursor.fetchone()
+    userKeys = ('id', "username", "pseudo", "created_at", "email")
+    userDict = dict(zip(userKeys, user))
+    response = {"message": "Ok", "data": {"id": video[0], "source": video[1], "created_at": video[2], "view": video[3], "enabled": video[4], "user" : userDict}}
+    return Response(response=json.dumps(response, default=str), status=201, content_type="application/json")
+
 @app.route('/video/<id>', methods=['DELETE'])
 def delete_video(id):
     cursor = mysql.connection.cursor()
